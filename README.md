017589e7af951a4a860f6b0dfd2e06cbf846e456
smvrnn <smvrnn@gmail.com>
2024-09-24 22:49:20 +0500

    add CommonJS support

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # VGSCode
+
 Validate Generate Sanitize Code
 
 List of supported codes:
+
 1. Kazakhstan: IIN, BIN
 2. ...
-
 
 ## Install
 
@@ -14,31 +15,41 @@ npm install vgscode
 pnpm install vgscode
 ```
 
-
 ## Usage ES6
+
 ```javascript
-import vgscode from 'vgscode';
+import vgscode from "vgscode";
+```
+
+## Usage Legacy
+
+```javascript
+const vgscode = require("vgscode");
 ```
 
 ## Example
 
 ### validate
+
 ```javascript
-vgscode.vBIN('320243026191') //true
-vgscode.vIIN('320229474023') //false
+vgscode.vBIN("320243026191"); //true
+vgscode.vIIN("320229474023"); //false
 ```
+
 ### generate
+
 ```javascript
-vgscode.gBIN('320243') //320243952485
-vgscode.gIIN('320229') //320229980830
+vgscode.gBIN("320243"); //320243952485
+vgscode.gIIN("320229"); //320229980830
 ```
+
 ### sanitize
 
 ```javascript
-vgscode.sNumberToString(320229474021) //320229474021
-vgscode.sTrimStart('  320229474021') //320229474021
-vgscode.sTrimEnd('320229474021   ') //320229474021
-vgscode.sTrimBoth('  320229474021  ') //320229474021
-vgscode.sTrimAll('32 0229  4740 21') //320229474021
-vgscode.sRemoveNonDigits('320#%2294 74fwef021') //320229474021
+vgscode.sNumberToString(320229474021); //320229474021
+vgscode.sTrimStart("  320229474021"); //320229474021
+vgscode.sTrimEnd("320229474021   "); //320229474021
+vgscode.sTrimBoth("  320229474021  "); //320229474021
+vgscode.sTrimAll("32 0229  4740 21"); //320229474021
+vgscode.sRemoveNonDigits("320#%2294 74fwef021"); //320229474021
 ```
--- src/sanitizers.js
+++ src/sanitizers.js
@@ -1,52 +1,59 @@
 export function sNumberToString(str) {
-if (typeof(str) === "number") {
-    return str = str + ''
-} else if (typeof(str) === "string") {
-    return str
-} else {
-    throw new TypeError(`Expected a 'string'/number but received a ${typeof(str)}`);
-}
+  if (typeof str === "number") {
+    return (str = str + "");
+  } else if (typeof str === "string") {
+    return str;
+  } else {
+    throw new TypeError(
+      `Expected a 'string'/number but received a ${typeof str}`
+    );
+  }
 }
 
 export function sTrimStart(str) {
-    var i = 0;
-    var ws = /\s/;
-    
-    while (ws.test(str.charAt(i))) {
-        i++;
-    }
-    
-    return str.slice(i);
+  var i = 0;
+  var ws = /\s/;
+
+  while (ws.test(str.charAt(i))) {
+    i++;
+  }
+
+  return str.slice(i);
 }
 
 export function sTrimEnd(str) {
-    var i = str.length - 1;
-    var ws = /\s/;
-    
-    while (ws.test(str.charAt(i))) {
-        i--;
-    }
-    
-    return str.slice(0, i + 1);
+  var i = str.length - 1;
+  var ws = /\s/;
+
+  while (ws.test(str.charAt(i))) {
+    i--;
+  }
+
+  return str.slice(0, i + 1);
 }
 
-export function sTrimBoth (str) {
-	var	str = str.replace(/^\s\s*/, ''),
-		ws = /\s/,
-		i = str.length;
-	while (ws.test(str.charAt(--i)));
-	return str.slice(0, i + 1);
+export function sTrimBoth(str) {
+  var str = str.replace(/^\s\s*/, ""),
+    ws = /\s/,
+    i = str.length;
+  while (ws.test(str.charAt(--i)));
+  return str.slice(0, i + 1);
 }
 // https://blog.stevenlevithan.com/archives/faster-trim-javascript
 
 export function sTrimAll(str) {
-    return str.replace(/\s/g, '');
+  return str.replace(/\s/g, "");
 }
 
 export function sRemoveNonDigits(str) {
-    return str.replace(/\D/g, '');
+  return str.replace(/\D/g, "");
 }
 
-
-export default { sNumberToString, sTrimStart, sTrimEnd, sTrimBoth, sTrimAll, sRemoveNonDigits };
-//module.exports = { sNumberToString, sTrimStart, sTrimEnd, sTrimBoth, sTrimAll, sRemoveNonDigits };
+export default {
+  sNumberToString,
+  sTrimStart,
+  sTrimEnd,
+  sTrimBoth,
+  sTrimAll,
+  sRemoveNonDigits,
+};
